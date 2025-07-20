0a620acff2f296b24370110d0a72e1396fe5cecd
Parth Patel <136750327+parth49patel@users.noreply.github.com>
2025-07-20 17:25:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ st.write('The name on smoothie will be:', name_on_oder)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -40,7 +40,7 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information') 
-        smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
